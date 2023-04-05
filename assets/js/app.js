0b5ad80da82c7cbf6dbcf3c5330f85e5137c99f8
safiabano <safiabano736@gmail.com>
2023-04-05 18:01:58 +0500

    class 2 practice

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+// let data="Hello ";
+// let data2="Students";
+// let data3= data+data2;
+// console.log(data3); //string + string concatination
+
+// let data="Hello ";
+// let data2=5;
+// let data3=data+data2;
+// console.log(data3); //string + number concatination
+
+// let data=5;
+// let data2=5;
+// let data3=data+data2;
+// console.log(data3); //  num+num Addition
+
+// let data="5";
+// let data2=5;
+// let data3= data+data2;
+// console.log(data3);  //string + num concatination
+
+// let data="Hello Students";
+// let data2=data.includes("S");
+// console.log(data2); //true or false
+//MATHS OPERTORS= + - * / % percentage sign called modullo
+
+// let data=19;
+// let data2=2;
+// console.log(data%data2) //modullo provide Remainder
+
+// let userFavNum=prompt("what is your favourite number?");
+// console.log(userFavNum);
+
+// let userName=prompt("what is your name?");
+// console.log("welcome to our website " + userName);
+
+// let randomNumber=Math.random();
+// // randomNumber=randomNumber *10;
+// let roundedNumber = Math.round(randomNumber);
+// console.log(roundedNumber);
+
+//short hand method
+//randomNumber *=10
